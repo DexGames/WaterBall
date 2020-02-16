7c3a1ebc3f6337da95809bd954de10d27d847458
Moto0116 <moto01160902@gmail.com>
2020-02-16 23:01:53 +0900

    ゲルストナー波Plane追加

---
+++ WaterBall/Assets/WaterSample/WaterPlane03/Water/WaterPlane03.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPlane03 : MonoBehaviour
+{
+    private Material material = null;
+
+    void Start()
+    {
+        this.material = GetComponent<MeshRenderer>().material;
+    }
+
+    void OnDestroy()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
